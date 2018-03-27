c664dde2b7796ff01d136963ff6aa05ae1d3ce88
Ricardo Antonio Pizano Pérez <rikymon2@hotmail.com>
2018-03-27 09:47:06 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'django_basic_crud',
+  packages = ['django_basic_crud'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple Django app to basic CRUD.',
+  author = 'Ricardo Pizano',
+  author_email = 'rikymon2@hotmail.com',
+  url = 'https://github.com/RicardoPizano/basic-crud', # use the URL to the github repo
+  download_url = 'https://github.com/RicardoPizano/basic-crud/tree/master/dist/django-basic-crud-0.2.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
